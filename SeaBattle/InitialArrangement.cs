0ea38a78b60ab063186aa7d58ab947f02eff5ac6
IceStorman <vladislavmartinisin@gmail.com>
2022-04-26 15:22:24 +0300

    Init project

---
+++ SeaBattle/InitialArrangement.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle
+{
+    public class InitialArrangement
+    {
+        private static int numberOfCells = 10;
+
+        private static char wall = '#';
+        private static char emptyCell = ' ';
+
+        public static char[,] FieldGenerating()
+        {
+            char[,] field = new char[numberOfCells, numberOfCells];
+            char cell = emptyCell;
+
+            for(int i = 0; i < numberOfCells; i++)
+            {
+                for(int j = 0; j < numberOfCells; j++)
+                {
+                    if(i == 0)
+                    {
+                        if (j < numberOfCells - 1) cell = Convert.ToChar(j.ToString());
+                        else cell = wall;
+                    }
+                    else if(i < numberOfCells - 1)
+                    {
+                        if (j == 0) cell = Convert.ToChar(i.ToString());
+                        else if (j < numberOfCells - 1) cell = emptyCell;
+                        else cell = wall;
+                    }
+                    else
+                    {
+                        cell = wall;
+                    }
+
+                    field[j, i] = cell;
+                }
+            }
+            return field;
+        }
+
+        public static void FieldDrawing(char[,] field)
+        {
+            char cell = emptyCell;
+
+            for(int i = 0; i < numberOfCells; i++)
+            {
+                for(int j = 0; j < numberOfCells; j++)
+                {
+                    cell = field[j, i];
+                    Console.Write(cell);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ SeaBattle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle
+{
+    internal class Program
+    {
+        private static char[,] firstField = InitialArrangement.FieldGenerating();
+        private static char[,] secondField = InitialArrangement.FieldGenerating();
+
+        static void Main(string[] args)
+        {
+            while (!IsGameEnd())
+            {
+                InitialArrangement.FieldDrawing(firstField);
+                InitialArrangement.FieldDrawing(secondField);
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        private static bool IsGameEnd()
+        {
+            return false;
+        }
+    }
+}
